489b346468ff238a37cc444acf8e81ba0b6c433f
Mahedi Hasan Bijoy <64260243+Mahedi2150@users.noreply.github.com>
2021-11-25 02:32:56 +0600

    img design

--- src/Components/PlaceDetails/PlaceDetails.js
+++ src/Components/PlaceDetails/PlaceDetails.js
@@ -31,7 +31,7 @@ const PlaceDetails = () => {
                             <div className="card " style={{ maxWidth: "450px" }}>
                                 <div className="row g-0">
                                     <div className="">
-                                        <img src={bikes?.img} style={{ height: "250px" }} className="img-fluid rounded-start w-100" alt="..." />
+                                        <img src={bikes?.img} style={{ objectFit: "cover", height: "350px" }} className="img-fluid rounded-start w-100" alt="..." />
                                     </div>
                                     <div className="">
                                         <div className="card-body">
